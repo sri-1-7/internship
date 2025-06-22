047b1c127ebeee0cf833818aab83d59d868ac3be
sri-1-7 <sriarthilk@gmail.com>
2025-06-22 20:00:22 +0530

    duplicate.c changed

--- dupilcate.c
+++ dupilcate.c
@@ -12,34 +12,36 @@ int compare(const void *num1,const void *num2)
 //main function
 void main()
 {
-    int array[MAX];
-    int length;
-    printf("Enter the no of elements:");
-    scanf("%d",&length);
+    int array[MAX];//array constant
+    int length;//to store length of array
+    //counter variables
     int counter1;
     int counter2;
-    int counter3;
+
+    //reading the elements and length
+    printf("Enter the no of elements:");
+    scanf("%d",&length);
     printf("Enter the elements:\n");
     for(counter1=0;counter1<length;counter1++)
     {
         scanf("%d",&array[counter1]);
     }
+
+    //sorting and removing duplicates
     qsort(array,length,sizeof(int),compare);
-    for(counter1=0;counter1<length;counter1++)
+    counter2=0;
+    for(counter1=1;counter1<length;counter1++)
     {
-       for(counter2=counter1+1;counter2<length;counter2++)
-       {
-         if(array[counter1]==array[counter2])
-         {
-            for(counter3=counter2;counter3<length-1;counter3++)
-            {
-                array[counter3]=array[counter3+1];
-            }
-            length--;
-        } 
-       }
+        if(array[counter1]!=array[counter2])
+        {
+            counter2++;
+            array[counter2]=array[counter1];
+        }
     }
-    for(counter1=0;counter1<length;counter1++)
+    //printing the elements
+    printf("%d ",counter2);
+    printf("The sorted array withou duplicate:\n");
+    for(counter1=0;counter1<=counter2;counter1++)
     {
         printf("%d ",array[counter1]);
     }
