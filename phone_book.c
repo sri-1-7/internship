e99c90144695e430139a5320e2b66b8bad382a4b
sri-1-7 <sriarthilk@gmail.com>
2025-06-30 11:00:08 +0530

    modified

--- phone_book.c
+++ phone_book.c
@@ -1,17 +1,13 @@
-//header file
 #include<stdio.h>
 #include<stdlib.h>
 
 struct node
 {
-    char name[30];
-    long long int roll_no;
-    int m1;
-    int m2;
-    int m3;
-    int total;
+    long long int num;
+    char name[20];
     struct node *link;
 };
+
 //function definition
 void create_node(struct node **old)//function to create linked list
 {   
@@ -24,11 +20,8 @@ void create_node(struct node **old)//function to create linked list
         printf("name:");
         char ch=getchar();
         gets(fresh->name);
-        printf("roll no:");
-        scanf("%lld",&fresh->roll_no);
-        printf("Enter the three marks:");
-        scanf("%d%d%d",&fresh->m1,&fresh->m2,&fresh->m3);
-        fresh->total=fresh->m1+fresh->m2+fresh->m3;
+        printf("num:");
+        scanf("%lld",&fresh->num);
         fresh->link=NULL;
         (*old)->link=fresh;
         *old=(*old)->link;
@@ -36,32 +29,29 @@ void create_node(struct node **old)//function to create linked list
         scanf("%d",&counter);
     }
 }
-//main function
 void main()
 {
-    struct node *head,*old,*traverse;
+    struct node *head;
+    struct node *old;
+    struct node *traverse;
     int counter;
     head=malloc(sizeof(struct node));
-    printf("Enter the data\n");
-    printf("name:");
+    printf("Enter the name:");
     gets(head->name);
-    printf("roll no:");
-    scanf("%lld",&head->roll_no);
-    printf("Enter the three marks:");
-    scanf("%d%d%d",&head->m1,&head->m2,&head->m3);
-    head->total=head->m1+head->m2+head->m3;
-    old=head;
+    printf("Enter the number:");
+    scanf("%lld",&head->num);
     head->link=NULL;
+    old=head;
     printf("if u want to enter one more value [1-yes/0-no]:");
     scanf("%d",&counter);
     if(counter=1)
     {
     create_node(&old);
-    printf("Entered datas");
+    }
     traverse=head;
     while(traverse!=NULL)
     {
-        printf("name:%s\nroll no:%lld\nmark1:%d\nmark2:%d\nmark3:%d\ntotal:%d\n\n",traverse->name,traverse->roll_no,traverse->m1,traverse->m2,traverse->m3,traverse->total);
+        printf("name:%s\nno:%lld\n",traverse->name,traverse->num);
         traverse=traverse->link;
     }
 }
